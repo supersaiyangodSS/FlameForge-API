76c98516310314241ba6d90036cf954b6575f1d9
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2023-10-15 23:18:45 +0530

    frontend

--- public/js/script.js
+++ public/js/script.js
@@ -1,42 +1,77 @@
-const navLinks = {
-    characters: document.getElementById('nav-characters'),
-    weapons: document.getElementById('nav-weapons'),
-    artifacts: document.getElementById('nav-artifacts')
-  };
-  
-  const contentData = document.getElementById('content-data');
-  const heading = document.getElementById('heading');
-  
-  const data = {
-    characters: `<h1 class="bg-teal-500">hello world</h1>
-      <input type="text" name="name">
-      <button class="btn">send</button>`,
-    weapons: 'weapons div',
-    artifacts: 'artifacts div'
-  };
-  
-  function showContent(item) {
-    contentData.innerHTML = data[item];
-  }
-  
-  function handleNavClick(item, link) {
-    showContent(item);
-    heading.innerHTML = link.innerHTML;
-  
-    for (const navLink of Object.values(navLinks)) {
-      navLink.classList.remove('light-gray');
+const characters = document.getElementById('nav-1');
+const weapons = document.getElementById('nav-2');
+const artifacts = document.getElementById('nav-3');
+const home = document.getElementById('nav-0');
+
+const contentData = document.getElementById('content-data');
+const heading = document.getElementById('heading');
+
+const homeContent = document.getElementById('content-0');
+const charactersContent = document.getElementById('content-1');
+const weaponsContent = document.getElementById('content-2');
+const artifactsContent = document.getElementById('content-3');
+
+function showTitle(title) {
+    heading.innerText = title;
+}
+
+const characterTitle = characters.innerText; 
+const weaponsTitle = weapons.innerText;
+const artifcatsTitle = artifacts.innerText;
+
+showTitle(characterTitle)
+
+function loadDefaults() {
+    // characters.classList.add('light-gray')
+    showTitle('');
+    charactersContent.style.display = 'none';
+    weaponsContent.style.display = 'none';
+    artifactsContent.style.display = 'none'
+    homeContent.style.display = 'block';
+}
+loadDefaults();
+
+function toggleActive(navOrder) {
+    for (let i = 1; i <= 3; i++) {
+            document.getElementById(`nav-${i}`).classList.remove('light-gray');
+        }
+    document.getElementById(`nav-${navOrder}`).classList.add('light-gray');
+}
+
+function toggleContent(contentOrder) {
+    for (let i = 0; i <=3; i++) {
+        document.getElementById(`content-${i}`).style.display = 'none';
     }
-    navLinks[item].classList.add('light-gray');
-  }
-  
-  for (const [item, navLink] of Object.entries(navLinks)) {
-    navLink.addEventListener('click', () => {
-      handleNavClick(item, navLink);
-    });
-  }
-  
-  // Initial content display
-  showContent('characters');
-  heading.innerHTML = navLinks.characters.innerHTML;
-  navLinks.characters.classList.add('light-gray');
-  
+    document.getElementById(`content-${contentOrder}`).style.display = 'block';
+}
+
+home.addEventListener('click', () => {
+    toggleContent(0);
+    showTitle('');
+})
+
+characters.addEventListener('click', () => {
+    toggleContent(1);
+    toggleActive(1)
+    showTitle(characterTitle)
+});
+
+weapons.addEventListener('click', () => {
+    toggleContent(2);
+    toggleActive(2)
+    showTitle(weaponsTitle)
+});
+
+artifacts.addEventListener('click', () => {
+    toggleContent(3);
+    toggleActive(3)
+    showTitle(artifcatsTitle)
+});
+
+
+//test only
+charactersContent.style.display = 'block'
+homeContent.style.display = 'none'
+
+const copyBtn = document.getElementById('copy-btn');
+const codeBox = document.getElementById('codeBox');
