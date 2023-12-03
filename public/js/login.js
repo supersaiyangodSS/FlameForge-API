deee2c483df87569301d7e72c6175eb9820ed437
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2023-12-03 15:23:10 +0530

    add login,register/success,error toast card

--- public/js/login.js
+++ public/js/login.js
@@ -1,14 +1,28 @@
 let alertBoxTimer;
-function showAlertBoxLogin(msg) {
-    const alertBox = document.getElementById('alertBoxLogin');
+function showAlertBox(msg) {
+    const alertBox = document.getElementById('alertBox');
     alertBox.textContent = msg;
     alertBox.style.top = '3%'
     if (alertBoxTimer) {
         clearInterval(alertBoxTimer);
     }
-    alertBoxTimer= setInterval(() => {
+    alertBoxTimer = setInterval(() => {
         alertBox.style.top = '-50%';
         clearInterval(alertBoxTimer)
+    }, 3000);
+}
+
+let alertBoxErrorTimer;
+function showAlertErrorBox(msg) {
+    const alertBox = document.getElementById('alertBoxError');
+    alertBox.textContent = msg;
+    alertBox.style.top = '3%'
+    if (alertBoxErrorTimer) {
+        clearInterval(alertBoxErrorTimer);
+    }
+    alertBoxErrorTimer = setInterval(() => {
+        alertBox.style.top = '-50%';
+        clearInterval(alertBoxErrorTimer)
     }, 3000);
 }
 
