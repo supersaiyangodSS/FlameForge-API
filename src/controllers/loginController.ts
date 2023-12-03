ee04532184efe5ee6247621f80a56be47dd1676b
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2023-12-03 19:48:10 +0530

    fix controller/login

--- src/controllers/loginController.ts
+++ src/controllers/loginController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import User from '../models/userModel.js';
 import { validationResult } from 'express-validator';
+import { logger } from "../helpers/logger.js";
 
 const loginPage = (req: Request, res: Response) => {
     res.render('login', {
         title: 'FlameForgeAPI Login',
         messages: req.flash(),
-    });
+    }); 
 }
 
 const loginUser = async (req: Request, res: Response) => {
@@ -15,37 +16,33 @@ const loginUser = async (req: Request, res: Response) => {
     console.log(`${email} and ${password}`)
     console.log(req.body)
     if (!errors.isEmpty()) {
-        console.log('validation error')
-        return res.status(409).json({
-            errors: errors.array().map((key) => key.msg)
-        });
+        const errorOne = errors.array()[0].msg;
+        req.flash('error', errorOne);
+        return res.status(301).redirect(`/sign-in`);
     }
     try {
         const user = await User.findOne({email})
         if (!user) {
-            return res.status(404).json({
-                notFound: `${email} does not exits`
-            });
+            req.flash('error', 'User does not exist!');
+            return res.status(301).redirect('/sign-in');
         }
         if (password === user.password) {
-            return res.status(401).json({
-                unauthorized: 'wrong username or password'
-            });
+            req.flash('error', 'Wrong Username or Password!');
+            return res.status(301).redirect('/sign-in');
         }
         if (user.verified === false) {
-            return res.status(302).redirect('/verify');
+            return res.status(301).redirect('/verify');
         }
         req.session.user = user.username;
         req.session.role = user.role;
         req.session.uid = user._id;
-        console.log('session ${user.username}') //TODO: Remove Later
-        console.log(`session is `, req.session.user);
         return res.status(200).redirect('/dashboard');
     }
     catch (error) {
+        logger.error(`User: ${req.session.user}, Error occured while logging in: ${error}`);
         console.log(error);
-        res.status(500).json({
-            error: 'internal server error'
+        res.status(500).render('500', {
+            title: "Internal Server Error!",
         });
     }
 }
