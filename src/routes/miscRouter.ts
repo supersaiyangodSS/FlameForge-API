1ace4f4e8facfa6a44b2759d3663095fbda9b4bd
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2023-12-04 20:45:39 +0530

    add validation to miscRouter

--- src/routes/miscRouter.ts
+++ src/routes/miscRouter.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
-import { reportPage } from '../controllers/miscController.js';
+import { reportPage, sendReport } from '../controllers/miscController.js';
 import { body } from 'express-validator';
 const router : Router = Router();
 
 const validateReport = [
     body('name').notEmpty().withMessage('Invalid Name!'),
     body('email').isEmail().withMessage('Invalid Email!'),
-    body('url').isEmpty().isURL().withMessage('Invalid URL!'),
-    body('message').isEmpty().withMessage('Invalid Message')
+    body('errorUrl').isURL().withMessage('Invalid URL!'),
+    body('message').notEmpty().withMessage('Invalid Message')
 ]
 
 router.route('/report')
     .get(reportPage)
+    .post(validateReport, sendReport);
 
 export default router;
