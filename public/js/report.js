c78d943fb762667cb3a88546d6404af82d52cd5b
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2023-12-04 23:00:39 +0530

    add report form validation

--- public/js/report.js
+++ public/js/report.js
@@ -25,3 +25,32 @@ function showAlertErrorBox(msg) {
         clearInterval(alertBoxErrorTimer)
     }, 3000);
 }
+
+const iname = document.getElementById('name');
+const email = document.getElementById('email');
+const errorUrl = document.getElementById('errorUrl');
+const message = document.getElementById('message');
+const reportForm = document.getElementById('reportForm');
+
+reportForm.addEventListener('submit', (e) => {
+    let valid = true;
+    if (email.value === '' || !email.value.includes('@')) {
+        showAlertErrorBox('Invalid Email!');
+        valid = false;
+    }
+    else if (iname.value === '') {
+        showAlertErrorBox('Invalid Name!');
+        valid = false;
+    }
+    else if (errorUrl.value === '') {
+        showAlertErrorBox('Invalid URL!');
+        valid = false;
+    }
+    else if (message.value === '') {
+        showAlertErrorBox('Invalid Message!');
+        valid = false;
+    }
+    else if (!valid) {
+        e.preventDefault();
+    }
+});
