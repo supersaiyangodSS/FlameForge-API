33d987c0a2b0f282aeeca3c2c3b960b43715a195
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2023-12-14 15:17:59 +0530

    add loader to homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -20,3 +20,12 @@ hamburgerBtnClose.addEventListener('click', () => {
             sidebar.classList.remove('flex');
         });
     });
+
+function hideLoader() {
+    const loader = document.getElementById('loader');
+    loader.style.display = 'none';
+}
+
+window.addEventListener('load', () => {
+    hideLoader();
+});
