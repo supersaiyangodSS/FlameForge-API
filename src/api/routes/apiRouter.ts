79aa9c7e1587b371d8814029ede3b8398200a984
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-01-02 00:19:13 +0530

    add get character by name, random character, all characters

--- src/api/routes/apiRouter.ts
+++ src/api/routes/apiRouter.ts
@@ -1,9 +1,18 @@
-import { Router , Request, Response} from "express";
-import { getMain } from "../controllers/apiController.js";
+import { Router } from "express";
+import { getMain, getAllCharacters, getCharacterByName, getRandomCharacter } from "../controllers/apiController.js";
 
 const router = Router();
 
 router.route('/')
-        .get(getMain);
+        .get(getMain); // homepage 
+
+router.route('/characters')
+        .get(getAllCharacters) // all characters
+
+router.route('/character')
+        .get(getCharacterByName) // character by name
+
+router.route('/character/random')
+        .get(getRandomCharacter)
 
 export default router;
