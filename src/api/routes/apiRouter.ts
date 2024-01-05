91e67472dc259a2624a77b3385e8f63c93c34ad1
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-01-05 19:00:03 +0530

    add api router for weapon, artifact

--- src/api/routes/apiRouter.ts
+++ src/api/routes/apiRouter.ts
@@ -1,18 +1,31 @@
 import { Router } from "express";
-import { getMain, getAllCharacters, getCharacterByName, getRandomCharacter } from "../controllers/apiController.js";
+import { getMain } from "../controllers/apiController.js";
+import { getSingleCharacter, getAllCharacters } from "../controllers/characterController.js";
+import { getAllWeapons, getSingleWeapon } from "../controllers/weaponController.js";
+import { getAllArtifacts, getSingleArtifact } from "../controllers/artifactController.js";
+import { apiLimiter } from "../../helpers/limiter.js";
 
 const router = Router();
 
 router.route('/')
-        .get(getMain); // homepage 
+        .get(apiLimiter, getMain)
 
 router.route('/characters')
-        .get(getAllCharacters) // all characters
+        .get(apiLimiter, getAllCharacters)
 
 router.route('/character')
-        .get(getCharacterByName) // character by name
+        .get(apiLimiter, getSingleCharacter)
 
-router.route('/character/random')
-        .get(getRandomCharacter)
+router.route('/weapon')
+        .get(apiLimiter, getSingleWeapon)
+
+router.route('/weapons')
+        .get(apiLimiter, getAllWeapons)
+
+router.route('/artifact')
+        .get(apiLimiter, getSingleArtifact)
+
+router.route('/artifacts')
+        .get(apiLimiter, getAllArtifacts)
 
 export default router;
