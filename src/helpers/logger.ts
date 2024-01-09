3edbabdd4d2f46c593d37ca87852ece9157c25a5
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-01-09 11:17:39 +0530

    add route logger

--- src/helpers/logger.ts
+++ src/helpers/logger.ts
@@ -48,4 +48,21 @@ const apiLogger = winston.createLogger({
     ),
 });
 
-export { logger, apiLogger };
+const routeLogger = winston.createLogger({
+    transports: [
+        new winston.transports.File({ filename: 'logs/route/routeCalls.log', level: 'verbose', })
+    ],
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json(),
+        winston.format.printf(info => {
+            const ip = info.ip ? ` - IP: ${info.ip}` : '';
+            const method = info.method ? ` - Method ${info.method}`: '';
+            const endpoint = info.endpoint ? ` - Endpoint: ${info.endpoint}` : '';
+            const level = info.level ? ` - Level: ${info.level} ` : '';
+            return `${info.timestamp} ${info.level} ${info.message}${ip}${method}${endpoint}${level}`
+        })
+    ),
+});
+
+export { logger, apiLogger, routeLogger };
