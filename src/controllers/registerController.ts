0d49eb081bc44a9759743caaf57292b365b70fbc
vedantSSJ <81308923+supersaiyangodSS@users.noreply.github.com>
2024-01-15 19:12:09 +0530

    replace email verification link

--- src/controllers/registerController.ts
+++ src/controllers/registerController.ts
@@ -45,7 +45,7 @@ const addUser = async (req: Request, res: Response) => {
     const errors = validationResult(req);
     const { firstName, lastName, email, username, password } = req.body;
     const verificationToken : string = generateToken();
-    const verificationLink : string = `http://localhost:4000/sign-up/verify?token=${verificationToken}`;
+    const verificationLink : string = `https://5f75-45-115-89-2.ngrok-free.app/sign-up/verify?token=${verificationToken}`;
     if (!errors.isEmpty()) {
         const errorOne = errors.array()[0].msg;
             req.flash('error', errorOne);
